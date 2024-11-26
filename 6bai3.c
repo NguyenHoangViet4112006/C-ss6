165b85d28fdb778b18766de4920bb976b000fadb
NguyenHoangViet4112006 <viet4112006@gmail.com>
2024-11-26 22:05:40 +0700

    Add files via upload

---
+++ 6bai3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char matkhau = 9870;
+    char sonhap;
+
+    printf("nhap mat khau ");
+    scanf("%s", sonhap); 
+    if (sonhap = 9870 ) {
+        printf("mat khau dung");
+    } else {
+        printf("mat khau sai");
+    }
+
+    return 0;
+}
+
