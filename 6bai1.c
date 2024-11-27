09c5f6e67c1c6ced7cc97255acc54800c441cca0
NguyenHoangViet4112006 <viet4112006@gmail.com>
2024-11-27 10:31:16 +0700

    Update 6bai1.c

--- 6bai1.c
+++ 6bai1.c
@@ -4,16 +4,16 @@ int main() {
     int numbers[5];
     int sum = 0;
     int i;
-    printf("nhap 5 so nguyen:\n");
+    printf("nhap 5 so nguyen ");
     for (i = 0; i < 5; i++) {
         scanf("%d", &numbers[i]);
     }
     for (i = 0; i < 5; i++) {
         if (numbers[i] % 2 != 0) {
-            sum += numbers[i];
+            sum = sum + numbers[i];
         }
     }
-    printf("tong cac so le la %d\n", sum);
+    printf("tong cac so le la %d", sum);
 
     return 0;
 }
